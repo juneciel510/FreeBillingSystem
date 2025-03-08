cb3b99a24e6a6742f73bf5308096798dcaf0c927
Lejun Chen <chenlejun@gmail.com>
2025-03-09 00:39:31 +0100

    Add index page, Layout, ViewStart

--- FreeBilling/FreeBilling.Web/Program.cs
+++ FreeBilling/FreeBilling.Web/Program.cs
@@ -1,11 +1,29 @@
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-app.Run(async ctx =>
+if (builder.Environment.IsDevelopment())
 {
-    await ctx.Response.WriteAsync("<html><body><h1> Welcome to FreeBilling</h1></body></html>");
-});
+    app.UseDeveloperExceptionPage();
+}
+
+
+//Allow us to serve index.html as the default webpage
+//app.UseDefaultFiles();
+
+////Allows us to serve files from wwwroot folder
+//app.UseStaticFiles();
+
+app.MapRazorPages();
+
+//app.MapGet("/", () => "Hello World!");
+//app.Run(async ctx =>
+//{
+//    await ctx.Response.WriteAsync("<html><body><h1> Welcome to FreeBilling</h1></body></html>");
+//});
 
 
 app.Run();
