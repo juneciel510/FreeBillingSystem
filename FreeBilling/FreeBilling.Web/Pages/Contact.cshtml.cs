8ab87b3d5a6e49b70e5b176e29f3a1eedbac904f
Lejun Chen <chenlejun@gmail.com>
2025-03-10 14:15:47 +0100

    styling the layout, contact form etc

--- FreeBilling/FreeBilling.Web/Pages/Contact.cshtml.cs
+++ FreeBilling/FreeBilling.Web/Pages/Contact.cshtml.cs
@@ -3,41 +3,39 @@ using FreeBilling.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FreeBilling.Web.Pages
+namespace FreeBilling.Web.Pages;
+
+public class ContactModel : PageModel
 {
-    public class ContactModel : PageModel
+  private readonly IEmailService _emailService;
+
+  public ContactModel(IEmailService emailService)
+  {
+    _emailService = emailService;
+  }
+
+  public string Title { get; set; } = "Contact Me";
+  public string Message { get; set; } = "";
+
+  [BindProperty]
+  public ContactViewModel Contact { get; set; } = new ContactViewModel();
+
+  public void OnGet()
+  {
+  }
+
+  public void OnPost()
+  {
+    if (ModelState.IsValid)
     {
-        private IEmailService _emailService;
-
-        public ContactModel(IEmailService emailService)
-        {
-            _emailService = emailService;
-        }
-
-        public string Title { get; set; } = "Contact Me";
-        public string Message { get; set; } = "";
-
-        [BindProperty]
-        public ContactViewModel Contact { get; set; } = new ContactViewModel();
-        
-        public void OnGet()
-        {
-        }
-
-        public void OnPost()
-        {
-            if (ModelState.IsValid)
-            {
-                _emailService.SendMail("alice@nowhere.com",Contact.Email,Contact.Subject,Contact.Body);
-                Contact= new ContactViewModel();
-                ModelState.Clear();
-                Message = "Thank you for your message!";
-            }
-            else
-            {
-                Message = "Please fix the errors before sending.";
-            }
-            
-        }
+      _emailService.SendMail("bob@aol.com", Contact.Email, Contact.Subject, Contact.Body);
+      Contact = new ContactViewModel();
+      ModelState.Clear();
+      Message = "Sent...";
     }
+    else
+    {
+      Message = "Please fix the errors before sending.";
+    }
+  }
 }
