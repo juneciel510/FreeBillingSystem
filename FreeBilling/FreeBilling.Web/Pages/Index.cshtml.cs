961bdb01768ef848007c2b0f84a0e92d1184bf99
Lejun Chen <chenlejun@gmail.com>
2025-03-11 22:42:00 +0100

    use entity framework to build database (#3)
    * Build database, configurate, Add DBcontext, seeding database etc
    * introduce repository, logging etc

--- FreeBilling/FreeBilling.Web/Pages/Index.cshtml.cs
+++ FreeBilling/FreeBilling.Web/Pages/Index.cshtml.cs
@@ -1,12 +1,24 @@
+using FreeBilling.Data.Entities;
+using FreeBilling.Web.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace FreeBilling.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private IBillingRepository _repository;
+        public IEnumerable<Customer>? Customers { get; set; }
+
+        public IndexModel(IBillingRepository repository)
         {
+            _repository = repository;
+
+        }
+        public async Task OnGetAsync()
+        {
+            Customers = await _repository.GetCustomers();
         }
     }
 }
--- FreeBilling/FreeBilling.Web/Program.cs
+++ FreeBilling/FreeBilling.Web/Program.cs
@@ -1,7 +1,20 @@
 
+using FreeBilling.Web.Data;
 using FreeBilling.Web.Services;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+IConfigurationBuilder configBuilder = builder.Configuration;
+configBuilder.Sources.Clear();
+configBuilder.AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.Development.json", optional: true)
+    .AddUserSecrets(Assembly.GetExecutingAssembly())
+    .AddEnvironmentVariables()
+    .AddCommandLine(args);
+
+builder.Services.AddDbContext<BillingContext>();
+builder.Services.AddScoped<IBillingRepository, BillingRepository>();
 
 builder.Services.AddRazorPages();
 builder.Services.AddTransient<IEmailService, DevTimeEmailService>();
