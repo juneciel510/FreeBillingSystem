51d1a33701d6f26dae53bc5a62e24fa2599ce44a
Lejun Chen <chenlejun@gmail.com>
2025-03-23 15:08:21 +0100

    change one of the employee's email to be a registered email, migrate and update database for that change

---
+++ FreeBilling/FreeBilling.Web/Data/Migrations/20250323135939_UpdateEmployee.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreeBilling.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Email",
+                value: "chenlejun@gmail.com");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Email",
+                value: "john@freebilling.com");
+        }
+    }
+}
